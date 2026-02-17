b303ea6a4504af6d4a53132166e04ea8c7a9d6d4
Sr-Croqueta <118274139+Sr-Croqueta@users.noreply.github.com>
2026-02-17 14:22:22 +0100

    textos

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -60,7 +60,7 @@ export default function Navbar() {
             />
             <span className="leading-tight">
               <span className="block font-bold">
-                Vivienda Rural Huerta del Medio
+                Casa Rural Huerta del Medio
               </span>
               <span className="block text-xs opacity-70">Granada</span>
             </span>
